55eb1cfa795a80bb855ea77573a78b750d3e54d3
lGuy <luc.rosenzweig@yahoo.com>
2020-08-17 14:22:50 +0000

    Renamed for clarity

---
+++ api/register_server.php
@@ -0,0 +1,24 @@
+<?php
+// This script gets run everytime a server game starts running
+
+require_once "db_connect.php";
+
+$servername = $_GET['servername'];
+$remote_address = $_SERVER['REMOTE_ADDR'];
+
+$db_connection = create_connection();
+
+$check = $db_connection->prepare("select 1 from servers where servername = :servername;");
+$check->execute(['servername' => $servername]);
+
+if ($check->rowCount() > 0) {
+    print("0\n");
+}
+else {
+    $insert = $db_connection->prepare("insert into servers (servername, ip, playercount) values (:servername, :ip, 0)");
+    $insert->execute(['servername' => $servername, 'ip' => $remote_address]);
+
+    print("1\n");
+}
+
+?>
